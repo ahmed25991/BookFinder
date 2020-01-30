c9ece691bf9906b15a825893c31741c0f109736b
ahmed25991 <41265712+ahmed25991@users.noreply.github.com>
2020-01-30 09:36:17 +0200

    commit boo information

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,73 @@
+
+ 
+
 function searchBook(){
 
     var userSearch = document.getElementById('userInput').value ;
-    document.getElementById('result').innerHTML = userSearch ; 
+    var bookResult = document.getElementById('result') ; 
+
+
+    bookResult.innerHTML = "";
+
+    $.ajax({
+        type : "GET",
+        url : "https://www.googleapis.com/books/v1/volumes?q="+userSearch,
+        dataType : "JSON",
+        success : function (book){
+            console.log(book);
+            for(var i=0; i<book.items.length; i++){
+                
+                // create div for media container
+                var bookDiv = document.createElement('div');
+                bookDiv.className = 'media' ;
+
+                // create image for book
+                var image = document.createElement('img');
+                image.className = 'mr-3';
+                image.src = book.items[i].volumeInfo.imageLinks.thumbnail ;
+
+                // create div for title and description container
+                var bookInfoDiv = document.createElement('div');
+                bookInfoDiv.className ='media-body';
+
+                // create h5 for book title
+                var bookTitle = document.createElement('h5');
+                bookTitle.className = 'mt-0' ;
+                bookTitle.innerHTML = book.items[i].volumeInfo.title ;
+
+
+                var author = document.createElement('h4');
+                author.innerHTML = book.items[i].volumeInfo.authors ;
+
+                var country = document.createElement('p');
+                country.innerHTML ="Country:- " + book.items[i].accessInfo.country ;
+
+
+                var description = document.createElement('p');
+                description.innerHTML = book.items[i].volumeInfo.description;
+
+
+                // create horizontal line
+                var hrLine = document.createElement('hr');
+
+
+                // append title and description into book info div container
+                bookInfoDiv.appendChild(bookTitle);
+                bookInfoDiv.appendChild(author);
+                bookInfoDiv.appendChild(country);
+                bookInfoDiv.appendChild(description);
+
+                // append image to book div
+                bookDiv.appendChild(image);
+                // append title to book div
+                bookDiv.appendChild(bookInfoDiv);
+                // append book div to main div
+                bookResult.appendChild(bookDiv);
+                //append horizontal line to main dev
+                bookResult.appendChild(hrLine);
+
+            }
+        }
+    })
+
 }
